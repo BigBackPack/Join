3a2c59c65b8632b422ac6dca4cb238622de57be7
johnni5 <johnni5@proton.me>
2024-06-19 20:10:52 +0200

    "live-display of cards - main, check, Overlay- open, Db inconsistency"

--- js/board-script.js
+++ js/board-script.js
@@ -71,8 +71,6 @@ function summaryInit() {
  * 
  * Object.value() - fills in contactList[] with the indexed-data, without the keys from Firebase
  */
-
-
 async function loadContactsData(path) {
   try {
     const response = await fetch(`${BASE_URL}${path}.json`);
@@ -137,13 +135,24 @@ function startBoard(tasks) {
  */
 function renderLive(taskList) {
   // div-containers, where to generate the respective card in
-  const cardLiveDiv2 = document.getElementById('column-2');
   const cardLiveDiv3 = document.getElementById('column-3');
   const cardLiveDiv4 = document.getElementById('column-4');
   // respective js-anker to create the cards
-  cardLiveDiv2.innerHTML += renderLiveProgressCard(taskList);
+  renderLiveProgress(taskList);
   cardLiveDiv3.innerHTML += renderLiveFeedbackCard();
   cardLiveDiv4.innerHTML += renderLiveDoneCard();
+}
+
+
+function renderLiveProgress(tasks) {
+  const cardLiveDiv2 = document.getElementById('column-2');
+  for (let i = 0; i < tasks.length; i++) {
+    const element = tasks[i];
+    if (element[1].board == 'progress') {
+      cardLiveDiv2.innerHTML += renderLiveProgressCard(element,i);
+    } 
+    
+  }
 }
 
 
@@ -151,12 +160,12 @@ function renderLive(taskList) {
  * This function returns the html/css for a card.
  * @returns - the generated HTML/Css structure for set function.
  */
-function renderLiveProgressCard(tasks) {
-  // how often - depends on - 
+function renderLiveProgressCard(tasks,i) {
+  // tasks = taskList;
   return `<div
                   draggable="true"
                   id="card-kochwelt"
-                  onclick="renderLiveOverlayCard('progress', taskList)"
+                  onclick="renderLiveOverlayCard('progress', taskList, ${i})"
                   class="board-card"
                 >
                   <div class="main-card">
@@ -175,12 +184,12 @@ function renderLiveProgressCard(tasks) {
                     </svg>
                     <div class="card-info">
                       <span class="card-title"
-                        >${tasks[0]['1']['title']}</span
+                        >${tasks[1]['title']}</span
                       >
                       <br />
                       <br />
                       <span class="card-sub-title"
-                        >${tasks[0][1]['description']}</span
+                        >${tasks[1]['description']}</span
                       >
                       <br />
                       <br />
@@ -539,13 +548,13 @@ function renderLiveDoneCard() {
  * This function decides/loads the respective sub-function as per param.
  * @param {string} param - the respective category: progress, feedback, done 
  */
-function renderLiveOverlayCard(param, tasks) {
+function renderLiveOverlayCard(param, tasks,i) {
   document.getElementById('overlay-card').classList.remove('d-none');
   const content = document.getElementById('overlay-card');
   content.innerHTML = '';
 
   if (param == 'progress') {
-    content.innerHTML = renderLiveOverlayCardProgress(tasks);
+    content.innerHTML = renderLiveOverlayCardProgress(tasks,i);
   } else if (param == 'done') {
     content.innerHTML = renderLiveOverlayCardDone();
   } 
@@ -556,7 +565,18 @@ function renderLiveOverlayCard(param, tasks) {
  * This function generates and returns the html/css-data for the overlay-card.
  * @returns - retrun of generated html/css for the respective overlay
  */
-function renderLiveOverlayCardProgress(tasks) {
+function renderLiveOverlayCardProgress(tasks,i) {
+  let subtasksHtml = tasks[i][1]['subtasks'].map(subtasks => {
+    return `
+        <div class="ol-sub-task">
+            <input class="ol-sub-task-checkbox" type="checkbox" onclick="updateBar('progress')">
+            <span>${subtasks['text']}</span>
+        </div>
+    `;
+  }).join('');
+
+  tasks = taskList;
+
   return `
     <div class="ol-board-card">
                 <div class="ol-main-card">
@@ -580,27 +600,27 @@ function renderLiveOverlayCardProgress(tasks) {
                   </div>
                   <div class="ol-card-info">
                     <span class="ol-card-title"
-                      >${tasks[0]['1']['title']}</span
+                      >${tasks[i][1]['title']}</span
                     >
                     <br />
                     <br />
                     <div class="ol-card-sub">
                       <span class="ol-card-sub-title"
-                        >${tasks[0][1]['description']}</span
+                        >${tasks[i][1]['description']}</span
                       >
                     </div>
                     <br />
                     <div class="ol-due-date">
                       <p>
                         Due date:
-                        <span class="ol-due-date-txt">${tasks[0][1]['dueDate']}</span>
+                        <span class="ol-due-date-txt">${tasks[i][1]['dueDate']}</span>
                       </p>
                     </div>
                     <br />
                     <div class="ol-priority">
                       <p>
                         Priority:
-                        <span class="ol-priority-txt">${tasks[0][1]['priority']}</span>
+                        <span class="ol-priority-txt">${tasks[i][1]['priority']}</span>
                         <span class="ol-priority-icon">
                           <svg
                             width="18"
@@ -631,21 +651,21 @@ function renderLiveOverlayCardProgress(tasks) {
                               <circle cx="16" cy="16" r="15.5" fill="#1FD7C1" stroke="white"/>
                               <path d="M8.13303 20.1904V11.4632H13.4001V12.4007H9.18985V15.3495H13.1273V16.287H9.18985V19.2529H13.4683V20.1904H8.13303ZM15.3049 11.4632H16.5663L19.5322 18.7075H19.6345L22.6004 11.4632H23.8617V20.1904H22.8731V13.5597H22.7879L20.0606 20.1904H19.106L16.3788 13.5597H16.2935V20.1904H15.3049V11.4632Z" fill="white"/>
                             </svg>
-                            <span class="ol-assigned-user-txt">${tasks[0]['assignment'][0]}</span>
+                            <span class="ol-assigned-user-txt">${tasks[1]['assignment']}</span>
                           </div>
                           <div class="ol-user">
                             <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <circle cx="16" cy="16" r="15.5" fill="#462F8A" stroke="white"/>
                             <path d="M7.81662 11.4632H9.07799L12.0439 18.7075H12.1462L15.1121 11.4632H16.3734V20.1904H15.3848V13.5597H15.2996L12.5723 20.1904H11.6178L8.89049 13.5597H8.80526V20.1904H7.81662V11.4632ZM18.4924 20.1904V11.4632H21.5435C22.1515 11.4632 22.6529 11.5683 23.0478 11.7785C23.4427 11.9859 23.7367 12.2657 23.9299 12.618C24.1231 12.9674 24.2197 13.3552 24.2197 13.7813C24.2197 14.1563 24.1529 14.466 24.0194 14.7103C23.8887 14.9546 23.7154 15.1478 23.4995 15.2899C23.2864 15.4319 23.0549 15.537 22.8049 15.6052V15.6904C23.072 15.7075 23.3404 15.8012 23.6103 15.9717C23.8802 16.1421 24.106 16.3865 24.2879 16.7046C24.4697 17.0228 24.5606 17.412 24.5606 17.8722C24.5606 18.3097 24.4612 18.7032 24.2623 19.0526C24.0634 19.4021 23.7495 19.6791 23.3205 19.8836C22.8916 20.0882 22.3333 20.1904 21.6458 20.1904H18.4924ZM19.5492 19.2529H21.6458C22.3362 19.2529 22.8262 19.1194 23.116 18.8524C23.4086 18.5825 23.5549 18.2558 23.5549 17.8722C23.5549 17.5768 23.4796 17.3041 23.3291 17.0541C23.1785 16.8012 22.964 16.5995 22.6856 16.449C22.4072 16.2955 22.0776 16.2188 21.697 16.2188H19.5492V19.2529ZM19.5492 15.2984H21.5095C21.8276 15.2984 22.1146 15.2359 22.3702 15.1109C22.6288 14.9859 22.8333 14.8097 22.9839 14.5825C23.1373 14.3552 23.214 14.0882 23.214 13.7813C23.214 13.3978 23.0805 13.0725 22.8134 12.8055C22.5464 12.5356 22.1231 12.4007 21.5435 12.4007H19.5492V15.2984Z" fill="white"/>
                             </svg>
-                            <span class="ol-assigned-user-txt">${tasks[0][1]['assignment'][1]['name']}</span>
+                            <span class="ol-assigned-user-txt">${tasks[1]['assignment']}</span>
                           </div>
                           <div class="ol-user">
                             <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                               <circle cx="16" cy="16" r="15.5" fill="#FF7A00" stroke="white"/>
                               <path d="M8.02224 20.1904H6.91428L10.1188 11.4632H11.2097L14.4143 20.1904H13.3063L10.6984 12.8438H10.6302L8.02224 20.1904ZM8.43133 16.7813H12.8972V17.7188H8.43133V16.7813ZM15.7737 11.4632H17.035L20.0009 18.7075H20.1032L23.0691 11.4632H24.3305V20.1904H23.3418V13.5597H23.2566L20.5293 20.1904H19.5748L16.8475 13.5597H16.7623V20.1904H15.7737V11.4632Z" fill="white"/>
                               </svg>
-                            <span class="ol-assigned-user-txt">${tasks[0][1]['assignment'][2]['name']}</span>
+                            <span class="ol-assigned-user-txt">${tasks[1]['assignment']}</span>
                           </div>
                             
                         </div>
@@ -655,28 +675,9 @@ function renderLiveOverlayCardProgress(tasks) {
                     <br />
                     <div class="ol-sub-tasks">
                       <span class="ol-sub-tasks-txt">Subtasks</span>
-                      <div class="ol-sub-tasks-container">
+                      <div id='display-sub-task' class="ol-sub-tasks-container">
 
-                        <div class="ol-sub-task">
-                          <input class="ol-sub-task-checkbox" type="checkbox" onclick="updateBar('progress')">
-                          <span>${tasks[0][1]['subtasks'][0]}</span>
-                        </div>
-                        <div class="ol-sub-task">
-                          <input class="ol-sub-task-checkbox" type="checkbox" onclick="updateBar('progress')">
-                          <span>${tasks[0][1]['subtasks'][1]}</span>
-                        </div>
-                        <div class="ol-sub-task">
-                          <input class="ol-sub-task-checkbox" type="checkbox" onclick="updateBar('progress')">
-                          <span>${tasks[0][1]['subtasks'][2]}</span>
-                        </div>
-                        <div class="ol-sub-task">
-                          <input class="ol-sub-task-checkbox" type="checkbox" onclick="updateBar('progress')">
-                          <span>${tasks[0][1]['subtasks'][3]}</span>
-                        </div>
-                        <div class="ol-sub-task">
-                          <input class="ol-sub-task-checkbox" type="checkbox" onclick="updateBar('progress')">
-                          <span>${tasks[0][1]['subtasks'][4]}</span>
-                        </div>
+                        ${subtasksHtml}
                         
                       </div>
                       <br>
@@ -887,12 +888,15 @@ function renderDummyOverlayCard(param) {
 //#region SUBTASK-COUNT
 let total = 0;
 
-function showSubTaskCount() {
+function showSubTaskCount(tasks) {
   total = 0; 
-  for (let i = 0; i < taskList.length; i++) {
-    const task = taskList[i];
+  for (let i = 0; i < tasks.length; i++) {
+    console.log('showSubCount - start FOR');
+    const task = tasks[i];
     const subtasks = task[1]['subtasks'];
-    total += subtasks.length;
+    total = subtasks.length;
+    console.log('showSubCount - total - ',total);
+    console.log('showSubCount - totalTasks - ',subtasks);
   }
   const sumSubTaskElements = document.getElementsByClassName('subtask-sum');
   for (let elem of sumSubTaskElements) {
@@ -925,4 +929,7 @@ function updateBar(type) {
 }
 //#endregion
 
+showSubTaskCount(taskList);
 
+//id=display-sub-task
+//--------------------------------------------------------------------------------
